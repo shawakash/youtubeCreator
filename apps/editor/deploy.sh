85f9edc8f39b6075ceb83c3344884e917b000873
Akash Shaw <shawakash2003@gmail.com>
2023-09-01 11:44:35 +0530

    Deploy.sh

--- apps/editor/deploy.sh
+++ apps/editor/deploy.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/youtubeCreator
 git pull origin main
+yarn install
 yarn build
 pm2 stop next
 pm2 start npm --name "next" -- run "start:editor"
