41dea8a280a3172d88ef547469eff1353b0a3e7f
Akash Shaw <shawakash2003@gmail.com>
2023-09-01 09:27:24 +0530

    Configuring Cors

---
+++ apps/editor/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+
+cd /home/ubuntu/youtubeCreator
+git pull origin main
+yarn build
+pm2 stop next
+pm2 start npm --name "next" -- run "start:next"
