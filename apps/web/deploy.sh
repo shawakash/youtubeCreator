9298bfe12c24c4f13482666d36f8387dacee2810
Akash Shaw <shawakash2003@gmail.com>
2023-09-09 21:52:58 +0530

    Checking node version

--- apps/web/deploy.sh
+++ apps/web/deploy.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin
 
 cd /home/ubuntu/youtubeCreator
 git pull origin main
+node -v 
 yarn install
 yarn build
 sudo rm -rf package-lock.json
