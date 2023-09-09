08eb4f6f2cca149e153500bf65834a01aa5466ed
Akash Shaw <shawakash2003@gmail.com>
2023-09-09 18:24:51 +0530

    New Instance

--- apps/editor/deploy.sh
+++ apps/editor/deploy.sh
@@ -4,6 +4,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 cd /home/ubuntu/youtubeCreator
 git pull origin main
 yarn install
+yarn build
 rm -rf package-lock.json
 pm2 stop editor
 pm2 start npm --name "editor" -- run "start:editor"
