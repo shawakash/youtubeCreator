e1120cda0693d1bb0d4e2631b426f40a56dd86d2
Akash Shaw <shawakash2003@gmail.com>
2023-09-09 19:22:25 +0530

    Cors

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui", "db", "zodTypes", "store", "tailwind-config"],
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "http://ec2-54-172-88-235.compute-1.amazonaws.com:3000" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
 }
 
 
