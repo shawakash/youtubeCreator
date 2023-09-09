0fdf5571509baa273037b9978c93535d1bf03764
Akash Shaw <shawakash2003@gmail.com>
2023-09-09 21:56:29 +0530

    Updating the node version

--- apps/editor/deploy.sh
+++ apps/editor/deploy.sh
@@ -4,6 +4,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin
 cd /home/ubuntu/youtubeCreator
 git pull origin main
 node -v 
+nvm use 20.6.1
 yarn install
 yarn build
 sudo rm -rf package-lock.json
